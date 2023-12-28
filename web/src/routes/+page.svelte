e3031b92a446f651a3c00fd74007311c463683eb
Guillermo <glexposito@gmail.com>
2023-12-29 10:38:36 +1300

    Add web site

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+  
+    let colors: string[];
+  
+    onMount(async () => {
+      const response = await fetch('http://0.0.0.0:8000/colors');
+      colors = await response.json();
+    });
+</script>
+
+<h1>Welcome to Svelte App!</h1>
+<h2>Show colors from API response!</h2>
+<h3>{colors}</h3>
